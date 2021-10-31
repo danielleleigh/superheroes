c88d8b756801a981d4107e0465d52c05a45094a9
danielleleigh <danielle.leigh90@gmail.com>
2021-10-31 13:18:16 -0500

    added URL for delete. attempted for update

--- superheroes/urls.py
+++ superheroes/urls.py
@@ -5,5 +5,7 @@ app_name = 'superheroes'
 urlpatterns = [
     path('', views.index, name='index'),
     path('<int:hero_id>', views.detail, name='detail'),
-    path('new/', views.create, name='create')
+    path('new/', views.create, name='create'),
+    path('delete/<int:hero_id>', views.delete, name='delete'),
+    path('update/<int:hero_id>', views.update, name='update'),
 ]
